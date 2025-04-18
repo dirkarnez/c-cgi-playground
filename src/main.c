92fca9ec0a2ff754121cb48544f577731fce3494
Dirk Arnez <smalldirkalex@gmail.com>
2025-04-18 15:34:48 +0800

    - update src/main.c

--- src/main.c
+++ src/main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+
 int main(void) {
   printf("Content-Type: text/plain;charset=us-ascii\n\n");
   printf("Hello world\n\n");
+  
+  /*
+  size_t size = sizeof(buffer);
+  fwrite(buffer, 1, size, stdout);
+  */
+
+
   return 0;
 }
